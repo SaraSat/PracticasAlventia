e70d62f19b3433f10d9fa412a4194e258b754097
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-03-23 12:48:08 +0100

    operaciones con dos numeros

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -8,5 +8,32 @@ function calculos(e){
             result[0].innerHTML+=x;
         else
             result[0].innerHTML=x;
+                                       
+       if(x=="="){
+           var operacion=result[0].innerHTML.split("=");
+          result[0].innerHTML+= resultado(operacion[0]);
+       }
+       if(x=="R"){
+           result[0].innerHTML="0";
+       }
+        
+
     }
 }
+    function resultado(op){
+        for(let i=0; i<op.length;i++){
+            if(op.charAt(i)=='+' ||op.charAt(i)=='-'||op.charAt(i)=='*'){
+                var operador=op.charAt(i);
+                op=op.split(operador);
+                switch(operador){
+                    case "+":
+                        return parseInt(op[0])+parseInt(op[1]);
+                    case "-":
+                        return parseInt(op[0])-parseInt(op[1]);
+                     case "*":
+                        return parseInt(op[0])*parseInt(op[1]);
+
+                }
+            }
+        }
+    }
