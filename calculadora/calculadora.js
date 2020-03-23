5f495c62fb3648fa81f9abe7eafcc8c84a7e7f75
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-03-23 13:48:44 +0100

    hecho lo basico

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,39 +1,19 @@
 document.getElementById("calculadora").addEventListener("click", calculos);
 
 function calculos(e){
-    if(e.target.className="button"){
-        var x=e.target.innerHTML;
+        
+         x=e.target.innerHTML;
         var result=document.getElementsByClassName('result');
         if(result[0].innerHTML!="0")
             result[0].innerHTML+=x;
         else
             result[0].innerHTML=x;
                                        
-       if(x=="="){
+       if(e.target.className=="calculate"){
            var operacion=result[0].innerHTML.split("=");
-          result[0].innerHTML+= resultado(operacion[0]);
+          result[0].innerHTML+= eval(operacion[0]);
        }
-       if(x=="R"){
+       if(e.target.className=="reset"){
            result[0].innerHTML="0";
        }
-        
-
-    }
 }
-    function resultado(op){
-        for(let i=0; i<op.length;i++){
-            if(op.charAt(i)=='+' ||op.charAt(i)=='-'||op.charAt(i)=='*'){
-                var operador=op.charAt(i);
-                op=op.split(operador);
-                switch(operador){
-                    case "+":
-                        return parseInt(op[0])+parseInt(op[1]);
-                    case "-":
-                        return parseInt(op[0])-parseInt(op[1]);
-                     case "*":
-                        return parseInt(op[0])*parseInt(op[1]);
-
-                }
-            }
-        }
-    }
