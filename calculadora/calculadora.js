933b8a734d05de36fe6405e5b0484083c04c95d1
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-03-24 09:52:46 +0100

    ejemplos de vue

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -2,18 +2,19 @@ document.getElementById("calculadora").addEventListener("click", calculos);
 
 function calculos(e){
         
-         x=e.target.innerHTML;
-        var result=document.getElementsByClassName('result');
+        var x=e.target.innerHTML; // x=7 x=* x=2 x='='-->clase calculate
+        var result=document.getElementsByClassName('result'); //donde escribe la operacion
         if(result[0].innerHTML!="0")
-            result[0].innerHTML+=x;
+            result[0].innerHTML+=x; // 7*2
         else
-            result[0].innerHTML=x;
+            result[0].innerHTML=x; //se escribe el 7 
                                        
-       if(e.target.className=="calculate"){
-           var operacion=result[0].innerHTML.split("=");
-          result[0].innerHTML+= eval(operacion[0]);
+       if(e.target.className=="calculate"){ //if(x=="=");
+           var operacion=result[0].innerHTML.split("="); // "7*2="=>operacion[0]=7*2 operacion[1]="";
+          result[0].innerHTML+= eval(operacion[0]); // a lo que hay escrito 7*2= se le añade el resultado de operacion[0]=7*2;
+          //el resultado= 7*2=14
        }
-       if(e.target.className=="reset"){
+       if(e.target.className=="reset"){ //if(x=="R")
            result[0].innerHTML="0";
        }
 }
